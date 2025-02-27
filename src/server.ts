3b95c24546e182eddbe93d0370918d0462410ad3
mclaughlin-thomas <thomas.mclaughlin2@stvincent.edu>
2025-02-27 10:23:36 -0500

    added express

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,18 @@
 import { createServer } from "http";
 import { handler } from "./handler";
+import { createServer as createHttpsServer } from
+"https";
+import { readFileSync } from "fs";
 const port = 5000;
+const https_port = 5500;
 const server = createServer(handler);
-server.listen(port, function() {
-    console.log(`Server listening on port ${port}`);
-});
+server.listen(port,() =>
+    console.log(`(Event) Server listening onport ${port}`));
+    const httpsConfig = {
+        key: readFileSync("key.pem"),
+        cert: readFileSync("cert.pem")
+};
+const httpsServer = createHttpsServer(httpsConfig, handler);
+httpsServer.listen(https_port, () =>
+    console.log(`HTTPS Server listening on port ${https_port}`)
+);
