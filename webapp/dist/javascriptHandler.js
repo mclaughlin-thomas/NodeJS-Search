048445d65c58f14b98551552b7df28ee7cc9a53d
mclaughlin-thomas <thomas.mclaughlin2@stvincent.edu>
2025-03-11 14:13:18 -0400

    fixed mime type error

--- webapp/dist/javascriptHandler.js
+++ webapp/dist/javascriptHandler.js
@@ -4,6 +4,7 @@ exports.javascriptHandler = void 0;
 const fs_1 = require("fs");
 const javascriptHandler = (req, resp) => {
     console.log("Serving search.js");
+    resp.setHeader("Content-Type", "application/javascript");
     resp.write((0, fs_1.readFileSync)("static/search.js"));
     resp.end();
 };
